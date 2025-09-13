0dee5a3bb7784367c790c1ef261261f101f1f32a
n-kats <n-kats19890214@hotmail.co.jp>
2025-09-13 15:24:47 +0900

    wip

--- samples/simple_sample.py
+++ samples/simple_sample.py
@@ -1,3 +1,10 @@
 from nk_autocode import autocode
 
-code = autocode("足し算", name="my_add", args=["a", "b"], return_type=int, interactive=True, verbose=True)
+my_add = autocode(
+    "足し算", name="my_add",
+    args=["a", "b"], return_type=int,
+    interactive=True, verbose=True)
+a = 5
+b = 3
+result = my_add(a, b)
+print(f"Result of adding {a} and {b} is: {result}")
